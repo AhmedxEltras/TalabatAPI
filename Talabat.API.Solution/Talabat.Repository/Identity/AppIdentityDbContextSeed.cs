fbcf481f9e65aeb1cba2b6971cce9602e92c936f
Ahmed Eltras <ahmedeltras10@gmail.com>
2024-09-29 04:52:19 +0200

    merge

---
+++ Talabat.API.Solution/Talabat.Repository/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Talabat.Core.Entities.Identity;
+
+namespace Talabat.Repository.Identity
+{
+    public static class AppIdentityDbContextSeed
+    {
+        public static async Task SeedUserAsync(UserManager<AppUser> userManager)
+        {
+            if(userManager.Users.Count() == 0)
+            {
+                var user = new AppUser()
+                {
+                    DisplayName = "Ahmed Eltras",
+                    Email = "ahmedeltras10@gmail.com",
+                    UserName = "ahmedeltras",
+                    PhoneNumber = "01018127525"
+                };
+                await userManager.CreateAsync(user, "P@$$w0rd");
+            }
+        }
+    }
+}
